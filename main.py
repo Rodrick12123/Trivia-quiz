8a959433a776bda43b8a31cc4145254f2e3a9c0d
Rodrick Lankford <lankfordr@carleton.edu>
2020-11-15 22:42:27 +0000

    added quiz questions

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from demo import demo
+from grammar.engine import GrammarEngine
 
-
-demo(random_seed=None)
+engine = GrammarEngine(
+    file_path='grammar/grammars/finalg.txt'
+    # initial_state=None,
+    # random_seed=random_seed
+)
+for _ in range(20):
+  print(engine.generate(start_symbol_name='test'))
